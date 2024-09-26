e8450c6841777e23cce530a903ac0d3b85282ee3
SonnyFixit <fikser@tlen.pl>
2024-09-27 01:16:40 +0200

    [Scripts] Check used libraries

---
+++ Scripts/NeuralNetworks/Cloud_NeuralNetworks/Library_Check.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import torch
+import psutil
+import sklearn
+
+print("Scikit-learn version:", sklearn.__version__)
+
+print("NumPy version:", np.__version__)
+print("Pandas version:", pd.__version__)
+print("PyTorch version:", torch.__version__)
+print("psutil version:", psutil.__version__)
