c07691ccf1430863ffa056b9356906fc4743c570
SonnyFixit <fikser@tlen.pl>
2024-09-28 13:22:18 +0200

    [Scripts] Removed old visualization scripts

--- Scripts/Utilities/Library_Check.py
+++ Scripts/Utilities/Library_Check.py
@@ -3,10 +3,15 @@ import pandas as pd
 import torch
 import psutil
 import sklearn
+import matplotlib
+import seaborn as sns
+import sys
 
+print("Python version:", sys.version)
 print("Scikit-learn version:", sklearn.__version__)
-
 print("NumPy version:", np.__version__)
 print("Pandas version:", pd.__version__)
 print("PyTorch version:", torch.__version__)
 print("psutil version:", psutil.__version__)
+print("Matplotlib version:", matplotlib.__version__)
+print("Seaborn version:", sns.__version__)
